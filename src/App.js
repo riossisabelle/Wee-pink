ef115f3c8e452264e84c11742114eec3c8bd32e0
riossisabelle <isabelleriossousa@gmail.com>
2025-04-30 08:19:26 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Promocoes from './components/Promocoes';
+
+function App() {
+  return (
+    <div className="App">
+      <Promocoes />
+    </div>
+  );
+  
+}
+
+
+
+export default App;
